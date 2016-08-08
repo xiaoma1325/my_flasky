31630d7aec57ab58b7624c99faca9d3e2e0e7f5b
xiaoma1325 <xiaoma1325@ihandysoftdeiMac-22.local>
2016-08-08 11:52:42 +0800

    Web forms with Flask-WTF (4a)

--- hello.py
+++ hello.py
@@ -1,22 +1,31 @@
-from datetime import datetime
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms.validators import Required
+from wtforms import StringField, SubmitField
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+    form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
